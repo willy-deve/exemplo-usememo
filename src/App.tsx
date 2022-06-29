2b4cb28e93c13431361796fe6807ffa0a7f98e1a
willy <willydasilva29@gmail.com>
2022-06-29 10:03:32 -0300

    codigo usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([""]);
+
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+  // const calculation = expensiveCalculation(count);
+
+  const increment = () => { setCount((c) => c + 1) };
+  const addTodo = () => { setTodos((t) => { return [...t, "oi"] }) };
+
+  return (<div>
+    <div>
+      <h2>Adicionar Elemento</h2>
+      {todos.map((todo, index) => { return <p key={index}>{todo}</p> })}
+      <button onClick={addTodo}>Adicionar</button>
     </div>
+    <hr />
+    <div>
+      Adicionar:  {count}
+      <button onClick={increment}> + </button>
+      <h2>Calculo</h2>
+      {calculation}
+    </div>
+  </div>
   );
-}
+};
+
+const expensiveCalculation = (num: number) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 10000000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
 
 export default App;
+
+
